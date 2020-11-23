75e40aca751738fa013da27e1f32790834a0f330
Kilah Willingham <kilahmay@kilahs-air.lan>
2020-11-23 15:30:13 -0800

    choreo page with map built

---
+++ src/pages/choreo.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { graphql, useStaticQuery, Link } from 'gatsby'
+import Img from 'gatsby-image'
+import Layout from '../components/layout'
+
+export default function Choreo() {
+  const data = useStaticQuery(graphql`
+    {
+      allContentfulChoreography {
+        edges {
+          node {
+            title
+            thumbnail {
+              fluid(maxWidth: 900, quality: 100) {
+                ...GatsbyContentfulFluid
+              }
+              id
+            }
+            director
+          }
+        }
+      }
+    }
+  `)
+  console.log(data)
+
+  //array
+  const choreo = data.allContentfulChoreography.edges
+
+  return (
+    <div>
+      <Layout>
+        <div>
+          {choreo.map((work, index) => (
+            <div key={index}>
+              <h3>{work.node.title}</h3>
+              <Img fluid={work.node.thumbnail.fluid} />
+              <h6>{work.node.director}</h6>
+            </div>
+          ))}
+        </div>
+      </Layout>
+    </div>
+  )
+}
---
+++ src/pages/directing.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import { graphql, useStaticQuery, Link } from 'gatsby'
+import Img from 'gatsby-image'
+import Layout from '../components/layout'
